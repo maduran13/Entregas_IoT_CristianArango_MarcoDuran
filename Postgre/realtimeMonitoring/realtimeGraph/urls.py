173454c431825b9864ebd5a1b2fe9268f94ad0e3
maduran13 <ma.duran13@uniandes.edu.co>
2022-07-12 02:25:22 -0500

    Se agrega timescale prueba 2

--- Postgre/realtimeMonitoring/realtimeGraph/urls.py
+++ Postgre/realtimeMonitoring/realtimeGraph/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('historical/data',
          download_csv_data, name='historical-data'),
+    path("mapJsonPlace/<str:measure>", get_json_by_place, name="mapJsonPlace")
 ]
